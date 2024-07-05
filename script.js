7916ff90e6d3998c7482e7c0100d0f7626b6dfea
Yashwanthmalviya <yashwanthmalviya1@gmail.com>
2024-07-05 09:25:28 +0530

    upgraded

--- script.js
+++ script.js
@@ -2,34 +2,37 @@
 //     if (e.propertyName !== 'transform') return;
 //     e.target.classList.remove('playing');
 //   }
-
 //   function playSound(e) {
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+//     const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
+//     const key = document.querySelector(`div[data-key="${e.keycode}"]`);
 //     if (!audio) return;
-
+  
 //     key.classList.add('playing');
 //     audio.currentTime = 0;
 //     audio.play();
 //   }
-
+  
 //   const keys = Array.from(document.querySelectorAll('.key'));
 //   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 //   window.addEventListener('keydown', playSound);
+  
+
 function removeTransition(e) {
-  if (e.propertyName !== 'transform') return;
-  e.target.classList.remove('playing');
-}
-function playSound(e) {
-  const audio = document.querySelector(`audio[data-key="${e.keycode}"]`);
-  const key = document.querySelector(`div[data-key="${e.keycode}"]`);
-  if (!audio) return;
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
 
-  key.classList.add('playing');
-  audio.currentTime = 0;
-  audio.play();
-}
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
-const keys = Array.from(document.querySelectorAll('.key'));
-keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-window.addEventListener('keydown', playSound);
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+
